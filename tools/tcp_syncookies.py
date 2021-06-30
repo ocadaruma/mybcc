381aa77ad0db6db554e2f5791d4ba0104e31a5c9
ocadaruma <ocadaruma@gmail.com>
2021-06-30 19:48:11 +0900

    show window clamp

--- tools/tcp_syncookies.py
+++ tools/tcp_syncookies.py
@@ -38,12 +38,43 @@ bpf_text += """
 struct event_t {
     u16 port;
     u8 rcv_wscale;
+    u32 window_clamp;
 };
 
+struct cookie_ctx_t {
+    u32 *window_clamp;
+};
+
+BPF_HASH(curr_cookie_ctx, u64, struct cookie_ctx_t);
 BPF_PERF_OUTPUT(events);
 """
 
 bpf_text += """
+int kprobe__cookie_v4_check(struct pt_regs *ctx,
+    struct sock *sk,
+    struct sk_buff *skb,
+    struct ip_options *opt) {
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+
+    struct cookie_ctx_t cookie_ctx = {};
+    curr_cookie_ctx.update(&pid_tgid, &cookie_ctx);
+    return 0;
+}
+
+int kprobe__tcp_select_initial_window(struct pt_regs *ctx,
+    int __space, __u32 mss,
+    __u32 *rcv_wnd, __u32 *window_clamp,
+    int wscale_ok, __u8 *rcv_wscale,
+    __u32 init_rcv_wnd) {
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+
+    struct cookie_ctx_t *cookie_ctx = curr_cookie_ctx.lookup(&pid_tgid);
+    if (cookie_ctx != 0) {
+        cookie_ctx->window_clamp = window_clamp;
+    }
+    return 0;
+}
+
 int kretprobe__cookie_v4_check(struct pt_regs *ctx) {
     struct sock *sk = (struct sock *)PT_REGS_RC(ctx);
     
@@ -56,13 +87,21 @@ int kretprobe__cookie_v4_check(struct pt_regs *ctx) {
     }
 
     struct tcp_sock *tp = (struct tcp_sock *)sk;
-    
+
     struct event_t event = {};
     event.port = ntohs(dport);
 
     u16 rx_opt_bits = 0;
     bpf_probe_read(&rx_opt_bits, sizeof(rx_opt_bits), &tp->rx_opt.opt_bits);
     event.rcv_wscale = rx_opt_bits >> 12;
+
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    struct cookie_ctx_t *cookie_ctx = curr_cookie_ctx.lookup(&pid_tgid);
+    if (cookie_ctx != 0) {
+        bpf_probe_read(&event.window_clamp, sizeof(u32), &cookie_ctx->window_clamp);
+        curr_cookie_ctx.delete(&pid_tgid);
+    }
+
     events.perf_submit(ctx, &event, sizeof(event));
 
     return 0;
@@ -80,15 +119,16 @@ bpf_text = bpf_text.replace('TARGET_HOST', str(
 def print_event(cpu, data, size):
     event = b["events"].event(data)
     printb(b"%-9s " % datetime.now().strftime("%H:%M:%S").encode('ascii'), nl="")
-    printb(b"%-7s %-10s" % (
+    printb(b"%-7s %-10s %-10s" % (
         event.port,
-        event.rcv_wscale
+        event.rcv_wscale,
+        event.window_clamp
     ))
 
 
 # header
-print("%-9s %-7s %-10s" % (
-    "TIME", "DPORT", "RCV_WSCALE"
+print("%-9s %-7s %-10s %-10s" % (
+    "TIME", "DPORT", "RCV_WSCALE", "WND_CLAMP"
 ))
 
 
